a16a68918248f1b5452f95f43896ca897259c27c
Miguel Miguez <miguelmiguezangel@gmail.com>
2023-08-23 15:57:12 -0300

    javascript

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/* Simulador */
+
+let zapatillasDestacadas = 80000   
+let zapatillasDestacadas2 = 80000   
+let zapatillasDestacadas3 = 80000   
+let zapatillasDestacadas4 = 80000 
+
+const saldo = 200000
+
+if( saldo <= zapatillasDestacadas){
+    console.log ("puede pagar")
+}else{
+    console.log ("no puede pagar")
+}
+    
+
+/* -------------------------------------- */
+
+const saldoefectivo = 10000
+const saldocredito = 90000
+
+if( zapatillasDestacadas + zapatillasDestacadas2 <= saldoefectivo ){
+    console.log ("no puede pagar")
+}else if (saldoefectivo + saldocredito >= zapatillasDestacadas ){
+    console.log ("puede pagar")
+} if ("puede pagar"){
+    console.log ("felicitaciones por la compra")
+}
+   
+
+
+/* -------------------------------------- */
+
+console.log( zapatillasDestacadas3 + zapatillasDestacadas4 )
+
+
+
+
+
