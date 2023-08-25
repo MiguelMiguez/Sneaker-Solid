b77c02f156fabbb77499f7bd8bdb177f500532bc
MiguelMiguez <miguelmiguezangel@gmail.com>
2023-08-25 15:28:55 -0300

    Update js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 /* Simulador */
 
-let zapatillasDestacadas = 80000   
-let zapatillasDestacadas2 = 80000   
-let zapatillasDestacadas3 = 80000   
-let zapatillasDestacadas4 = 80000 
 
-const saldo = 200000
+
+/* const saldo = 0;
 
 if( saldo <= zapatillasDestacadas){
     console.log ("puede pagar")
 }else{
     console.log ("no puede pagar")
 }
-    
+     */
 
 /* -------------------------------------- */
-
+/* 
 const saldoefectivo = 10000
 const saldocredito = 90000
 
@@ -25,15 +22,35 @@ if( zapatillasDestacadas + zapatillasDestacadas2 <= saldoefectivo ){
     console.log ("puede pagar")
 } if ("puede pagar"){
     console.log ("felicitaciones por la compra")
-}
+} */
    
 
 
 /* -------------------------------------- */
 
-console.log( zapatillasDestacadas3 + zapatillasDestacadas4 )
+let zapatillasDestacadas = 80000;
+let zapatillasDestacadas2 = 80000;
+let zapatillasDestacadas3 = 80000;
+let zapatillasDestacadas4 = 80000;
 
+const saldoTexto = prompt("Ingresa tu saldo disponible:");
+const saldo = parseFloat(saldoTexto);
+let puedepagar = false;
 
+if (saldo >= zapatillasDestacadas) {
+    console.log("Puede pagar");
+    puedepagar = true;
+} else {
+    console.log("No puede pagar");
+}
+
+if (puedepagar === true) {
+    console.log("Felicitaciones por la compra");
+} else {
+    console.log("Por favor carga el saldo nuevamente");
+}
+
+/* -------------------------------------- */
 
 
 
