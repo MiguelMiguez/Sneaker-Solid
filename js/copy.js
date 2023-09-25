9820075f5565c578fbb7346edcc5cee536c03f76
Miguel Miguez <miguelmiguezangel@gmail.com>
2023-09-25 16:00:05 -0300

    update js

--- js/copy.js
+++ js/copy.js
@@ -71,3 +71,38 @@ for(let i=1 ; i<=5 ; i+=1 ){
 
 /* -------------------------------------- */
 
+/* Simulador */
+
+const zapatillas1 = [
+    { id: 1, producto: "Zapatillas Nike", precio: 1000, stock: 3 },
+    { id: 2, producto: "Zapatillas Puma", precio: 2000, stock: 1 },
+    { id: 3, producto: "Zapatillas Adidas", precio: 5000, stock: 2 },
+];
+
+console.table(zapatillas1);
+
+const saldoTexto = prompt("Ingresa tu saldo disponible:");
+const saldo = parseFloat(saldoTexto);
+let puedepagar = false;
+
+for (let i = 0; i < zapatillas1.length; i++) {
+    const zapatilla = zapatillas1[i];
+    if (saldo >= zapatilla.precio && zapatilla.stock > 0) {
+        console.log(`Puede pagar las ${zapatilla.producto}`);
+        puedepagar = true;
+    }
+}
+
+if (puedepagar) {
+    console.log("Felicitaciones, tienes saldo suficiente para al menos una zapatilla.");
+    const seleccion = prompt("Selecciona la zapatilla que deseas comprar (ejemplo: Zapatillas Nike):");
+    const zapatillaSeleccionada = zapatillas1.find(zapatilla => zapatilla.producto === seleccion);
+
+    if (zapatillaSeleccionada) {
+        console.log(`Has seleccionado las ${zapatillaSeleccionada.producto} con un precio de ${zapatillaSeleccionada.precio}`);
+    } else {
+        console.log("La zapatilla seleccionada no está en el catálogo o no tienes saldo suficiente para comprarla.");
+    }
+} else {
+    console.log("Lo siento, no puedes comprar ninguna zapatilla con tu saldo actual.");
+}
--- js/main.js
+++ js/main.js
@@ -1,35 +1,28 @@
-/* Simulador */
+let articulosCarrito = [];
 
-const zapatillas1 = [
-    { id: 1, producto: "Zapatillas Nike", precio: 1000, stock: 3 },
-    { id: 2, producto: "Zapatillas Puma", precio: 2000, stock: 1 },
-    { id: 3, producto: "Zapatillas Adidas", precio: 5000, stock: 2 },
-];
+const listaProductos = document.querySelector('#allContent');
 
-console.table(zapatillas1);
+/* document.addEventListener('DOMContentLoaded', ()=>{
 
-const saldoTexto = prompt("Ingresa tu saldo disponible:");
-const saldo = parseFloat(saldoTexto);
-let puedepagar = false;
+})
+ */
 
-for (let i = 0; i < zapatillas1.length; i++) {
-    const zapatilla = zapatillas1[i];
-    if (saldo >= zapatilla.precio && zapatilla.stock > 0) {
-        console.log(`Puede pagar las ${zapatilla.producto}`);
-        puedepagar = true;
+listaProductos.addEventListener('click', agregarProducto);
+
+function agregarProducto(evt){
+    evt.preventDefault (); 
+    console.log(evt.target.classList.contains('agregar-carrito'))
+    if(evt.target.classList.contains('agregar-carrito')){
+        const producto = evt.target.parentElement.parentElement;
+        leerDatosProducto (producto)
     }
 }
 
-if (puedepagar) {
-    console.log("Felicitaciones, tienes saldo suficiente para al menos una zapatilla.");
-    const seleccion = prompt("Selecciona la zapatilla que deseas comprar (ejemplo: Zapatillas Nike):");
-    const zapatillaSeleccionada = zapatillas1.find(zapatilla => zapatilla.producto === seleccion);
-
-    if (zapatillaSeleccionada) {
-        console.log(`Has seleccionado las ${zapatillaSeleccionada.producto} con un precio de ${zapatillaSeleccionada.precio}`);
-    } else {
-        console.log("La zapatilla seleccionada no está en el catálogo o no tienes saldo suficiente para comprarla.");
-    }
-} else {
-    console.log("Lo siento, no puedes comprar ninguna zapatilla con tu saldo actual.");
+function leerDatosProducto(item){ 
+    const infoProducto = { 
+        modelo: item.querySelector('h1').textContent, 
+        genre: item.querySelector('h2').textContent, 
+        precio: item.querySelector('h3').textContent
+    } 
+    console.log(infoProducto) 
 }
