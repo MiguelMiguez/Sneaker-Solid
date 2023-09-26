eb0bbd609922a97ef46aaacaf75d072b3e6e5971
Miguel Miguez <miguelmiguezangel@gmail.com>
2023-09-26 11:02:13 -0300

    update

--- js/main.js
+++ js/main.js
@@ -8,6 +8,26 @@ console.log(listaProductos);
 })
  */
 
+listaProductos.addEventListener('click', agregarProducto)
+
+function agregarProducto(evt){
+    evt.preventDefault (); 
+    console.log(evt.target.classList.contains('agregar-carrito'))
+
+
+    if(evt.target.classList.contains('agregar-carrito')){
+        const producto = evt.target.parentElement.parentElement;
+        leerDatosProducto (producto)
+    } 
+}
 
 
 
+function leerDatosProducto(item){ 
+    const infoProducto = { 
+        modelo: item.querySelector('h2').textContent,  
+        genre: item.querySelector('h3').textContent,  
+        precio: item.querySelector('h4').textContent 
+    } 
+    console.log(infoProducto) 
+}
