26359c86807c6ab66fde5d3ec642424e88c22189
Miguel Miguez <miguelmiguezangel@gmail.com>
2023-09-26 10:17:29 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,28 +1,13 @@
 let articulosCarrito = [];
 
-const listaProductos = document.querySelector('#allContent');
+const listaProductos = document.querySelector('#lista-productos');
+console.log(listaProductos);
 
 /* document.addEventListener('DOMContentLoaded', ()=>{
 
 })
  */
 
-listaProductos.addEventListener('click', agregarProducto);
 
-function agregarProducto(evt){
-    evt.preventDefault (); 
-    console.log(evt.target.classList.contains('agregar-carrito'))
-    if(evt.target.classList.contains('agregar-carrito')){
-        const producto = evt.target.parentElement.parentElement;
-        leerDatosProducto (producto)
-    }
-}
 
-function leerDatosProducto(item){ 
-    const infoProducto = { 
-        modelo: item.querySelector('h1').textContent, 
-        genre: item.querySelector('h2').textContent, 
-        precio: item.querySelector('h3').textContent
-    } 
-    console.log(infoProducto) 
-}
+
